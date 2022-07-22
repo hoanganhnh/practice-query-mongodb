52fb8594cb63737eeba9e145ffa6a3a6016e6976
Nguyễn Hoàng Anh <hoahoanganh20012001@gmail.com>
2022-07-22 15:21:31 +0700

    demo with mongoose DB

--- app.js
+++ app.js
@@ -1,65 +1,67 @@
-import express from 'express'
-import { connectDB } from './db.js'
-import { ObjectId } from 'mongodb'
+import express from "express";
+import mongoose from "mongoose";
+
+import connect from "./db.js";
 
 // init app & middleware
-const app = express()
+const app = express();
 
 // db connection
-let db
+let db;
 
-connectDB.connectToDb((err) => {
-  if(!err){
-    app.listen('3000', () => {
-      console.log('app listening on port 3000')
-    })
-    db = connectDB.getDb()
-  }
-})
+const PORT = 5000;
 
+app.use(express.json());
+
+connect();
 // routes
-app.get('/posts', (req, res) => {
-  let books = []
+app.get("/", async (req, res) => {
+  const collection = mongoose.connection.db.collection("players");
 
-  db.collection('posts')
-    .find()
-    .sort({author: 1})
-    .forEach(book => books.push(book))
-    .then(() => {
-      res.status(200).json(books)
-    })
-    .catch(() => {
-      res.status(500).json({error: 'Could not fetch the documents'})
-    })
-})
+  const users = await collection
+    .aggregate([
+      {
+        $match: {
+          firstName: "Thien",
+          lastName: "Dang",
+        },
+      },
+    ])
+    .toArray();
 
-app.get('/posts/:id', (req, res) => {
+  res.status(200).json({
+    results: users.length,
+    data: users,
+  });
+});
 
+app.get("/posts/:id", (req, res) => {
   if (ObjectId.isValid(req.params.id)) {
-
-    db.collection('posts')
-      .findOne({_id: new ObjectId(req.params.id)})
-      .then(doc => {
-        res.status(200).json(doc)
+    db.collection("posts")
+      .findOne({ _id: new ObjectId(req.params.id) })
+      .then((doc) => {
+        res.status(200).json(doc);
       })
-      .catch(err => {
-        res.status(500).json({error: 'Could not fetch the document'})
-      })
-      
+      .catch((err) => {
+        res.status(500).json({ error: "Could not fetch the document" });
+      });
   } else {
-    res.status(500).json({error: 'Could not fetch the document'})
+    res.status(500).json({ error: "Could not fetch the document" });
   }
+});
 
-})
+app.post("/posts", (req, res) => {
+  const newBook = req.body;
+  db.collection("posts")
+    .insertOne(newBook)
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(500).json({ err: "Could not create a new doc" });
+    });
+});
 
-app.post('/posts', (req, res) => {
-    const newBook = req.body;
-    db.collection('posts')
-      .insertOne(newBook)
-      .then(result => {
-        res.status(200).json(result)
-      })
-      .catch(err => {
-        res.status(500).json({err: "Could not create a new doc"});
-      })
-})
+app.listen(PORT, () => {
+  console.log(`> Run on port http://localhost:${PORT}`);
+});
